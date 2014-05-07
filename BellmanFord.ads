60a5b391f930b4d54e15d936aae1787db02a8e7f
Thomas <thomas>
2014-05-07 13:37:43 +0200

    ajout des deux packages a faire

---
+++ BellmanFord.ads
@@ -0,0 +1,11 @@
+with Graphe;
+use Graphe;
+package BellmanFord is
+   
+   -- Calcul les chemins les plus du noeud vers tous les autres
+   procedure BellmanFord (G: Graphe; Source : Node);
+   
+   -- Affiche le chemin le plus court entre 2 noeuds
+   procedure AffichageBellmanFord (G: Graphe; Source : Node; Destination : Node);
+   
+end;
