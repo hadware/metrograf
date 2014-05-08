1eba124dddf1d25d8a585ba2e8ed5efb4c35a56b
super-moi <thomas>
2014-05-08 20:34:49 +0200

    ads

--- package_graphe.ads
+++ package_graphe.ads
@@ -4,7 +4,7 @@ with Ada.Integer_Text_Io;
 use Ada.Integer_Text_Io;
 with Ada.Strings.Unbounded;
 use Ada.Strings.Unbounded;
-with Ada.Containers.Doubly_Linked_Lists;
+
 
 package Package_Graphe is
         
@@ -13,24 +13,25 @@ package Package_Graphe is
       Station_Name : Unbounded_String;
    end record;
      
-
-   
    type P_Node is access Node;
+   type Node_Array is array (Integer range <>) of P_Node;
    
-   type vertex is record
+   type Vertex is record
       Source : P_Node;
       Destination : P_Node;
       Line : Unbounded_String;
       Cost : float;
    end record;
    
-   type P_vertex is access vertex;
-   
+   type P_Vertex is access Vertex;
+   type Vertex_Array is array (Integer range <>) of P_Vertex;
    
  
  --  package List is new Ada.Containers.Doubly_Linked_Lists(vertex);
   -- type Vertex_Lt is new List.List;
       
    type Graphe is array (Integer range <>, Integer range <>) of P_vertex; 
+   
+   procedure Get_Out_Put_Vertex ( N : P_Node; Array_Dest : out Node_Array);
    
 end;
