a00b08134aa6f1ad2d2a82262612a6bb643faa95
super-moi <thomas>
2014-05-14 20:04:35 +0200

    modif ads

--- bellmanford.ads
+++ bellmanford.ads
@@ -13,7 +13,7 @@ package BellmanFord is
    -- Calcul les chemins les plus du noeud vers tous les autres
    procedure Bellman ( Input_Graphe : Graphe; Source : Node; Cost_Array : out T_Cost_Array; Previous_Node_List : out Node_Id_Array);
    
-
+   
    
    -- Affiche le chemin le plus court entre 2 noeuds
    procedure AffichageBellmanFord (Input_Graphe : Graphe; Source : Node; Destination : Node; Node_A : P_Node_Array);
