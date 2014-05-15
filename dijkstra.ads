989c9ee6c754c5720d6c4b93f067e9bdd39bf1b6
super-moi <thomas>
2014-05-15 20:03:16 +0200

    debut debug dijkstra

--- dijkstra.ads
+++ dijkstra.ads
@@ -3,9 +3,9 @@ use Package_Graphe;
 package Dijkstra is
 
    -- Calcul le plus cours chemin d'un noeud source vers tout les autres noeuds du graphe
-   function Dijkstra (Input_Graph: Graphe; Source_Id : Integer) return Distance_Array;
+   function Dijkstra (Input_Graph: Graphe; Source_Id : Integer) return T_Cost_Array;
 
    -- Affiche le chemin le plus court entre 2 noeuds
-   procedure AffichageDijkstra (G: Graphe; Source : Node; Destination : Node);
+   procedure AffichageDijkstra (Input_Graph: Graphe; Source : Node; Destination : Node; Input_Node_Array : P_Node_Array);
 
 end;
