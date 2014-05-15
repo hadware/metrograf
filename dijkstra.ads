2006ef5784cd44665fb47a55615e719ae69a99a0
hadware <hadwarez@gmail.com>
2014-05-15 23:38:28 +0200

    Ptet c'es tmodif?

--- dijkstra.ads
+++ dijkstra.ads
@@ -3,7 +3,7 @@ use Package_Graphe;
 package Dijkstra is
 
    -- Calcul le plus cours chemin d'un noeud source vers tout les autres noeuds du graphe
-   function Dijkstra (Input_Graph: Graphe; Source_Id : Integer) return T_Cost_Array;
+   procedure Dijkstra (Input_Graph: Graphe; Source_Id : Integer; Previous_Nodes : in out Node_Id_Array; Distances : in out T_Cost_Array);
 
    -- Affiche le chemin le plus court entre 2 noeuds
    procedure AffichageDijkstra (Input_Graph: Graphe; Source : Node; Destination : Node; Input_Node_Array : P_Node_Array);
